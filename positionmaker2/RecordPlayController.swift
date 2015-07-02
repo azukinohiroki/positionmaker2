3958e26e83f34a3eeff7f0eb88488910e2d8ee9f
Hiroki <h.taoka@jwork.co.jp>
2015-07-02 13:45:39 +0900

    add RecordPlayController

---
+++ positionmaker2/RecordPlayController.swift
@@ -0,0 +1,13 @@
+//
+//  RecordPlayController.swift
+//  positionmaker2
+//
+//  Created by Hiroki Taoka on 2015/07/02.
+//  Copyright (c) 2015年 azukinohiroki. All rights reserved.
+//
+
+import Foundation
+
+class RecordPlayController {
+  
+}
